2d2d646923772025a4b12443e92cfc7dd5185405
tlong <t.long@yale.edu>
2025-09-16 15:36:57 -0400

    cleanup

--- main_classification.py
+++ main_classification.py
@@ -6,60 +6,71 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
 import wandb
 from utils.read_data import load_data
-from torchinfo import summary
 
 from models.graph_learning import HiPoNet, MLP
 from argparse import ArgumentParser
 
 import gc
+
 gc.enable()
 
 # Define the parameters using parser args
 parser = ArgumentParser(description="Pointcloud net")
-parser.add_argument('--raw_dir', type=str, default = 'COVID_data', help="Directory where the raw data is stored")
-parser.add_argument('--full', action='store_true')
-parser.add_argument('--task', type=str, default = 'prolif', help="Task on PDO data")
-parser.add_argument('--num_weights', type=int, default=2, help="Number of weights")
-parser.add_argument('--threshold', type=float, default= 0.5, help="Threshold for creating the graph")
-parser.add_argument('--sigma', type=float, default= 0.5, help="Bandwidth")
-parser.add_argument('--K', type=int, default= 1, help="Order of simplicial complex")
-parser.add_argument('--hidden_dim', type=int, default= 250, help="Hidden dim for the MLP")
-parser.add_argument('--num_layers', type=int, default= 3, help="Number of MLP layers")
-parser.add_argument('--lr', type=float, default= 0.01, help="Learnign Rate")
-parser.add_argument('--wd', type=float, default= 3e-3, help="Weight decay")
-parser.add_argument('--num_epochs', type=int, default= 20, help="Number of epochs")
-parser.add_argument('--batch_size', type=int, default= 32, help="Batch size")
-parser.add_argument('--gpu', type=int, default= 0, help="GPU index")
+parser.add_argument(
+    "--raw_dir",
+    type=str,
+    default="COVID_data",
+    help="Directory where the raw data is stored",
+)
+parser.add_argument("--full", action="store_true")
+parser.add_argument("--task", type=str, default="prolif", help="Task on PDO data")
+parser.add_argument("--num_weights", type=int, default=2, help="Number of weights")
+parser.add_argument(
+    "--threshold", type=float, default=0.5, help="Threshold for creating the graph"
+)
+parser.add_argument("--sigma", type=float, default=0.5, help="Bandwidth")
+parser.add_argument("--K", type=int, default=1, help="Order of simplicial complex")
+parser.add_argument(
+    "--hidden_dim", type=int, default=250, help="Hidden dim for the MLP"
+)
+parser.add_argument("--num_layers", type=int, default=3, help="Number of MLP layers")
+parser.add_argument("--lr", type=float, default=0.01, help="Learnign Rate")
+parser.add_argument("--wd", type=float, default=3e-3, help="Weight decay")
+parser.add_argument("--num_epochs", type=int, default=20, help="Number of epochs")
+parser.add_argument("--batch_size", type=int, default=32, help="Batch size")
+parser.add_argument("--gpu", type=int, default=0, help="GPU index")
 args = parser.parse_args()
 
-
-wandb.init(project='pointcloud-net-k-fold', 
-                 config = vars(args))
-
 if args.gpu != -1 and torch.cuda.is_available():
-    args.device = 'cuda'
+    print(f"Using {args.gpu} GPUs")
+    args.device = "cuda"
 else:
-    args.device = 'cpu'
+    args.device = "cpu"
 
 
 def test(model, mlp, PCs, labels, loader):
     model.eval()
     mlp.eval()
     correct = 0
-    total = 0                   
+    total = 0
     with torch.no_grad():
-        for idx in (loader):
+        for idx in loader:
             # X = model(idx, 0.000001)
             X = model([PCs[i].to(args.device) for i in idx], args.sigma)
             logits = mlp(X)
             preds = torch.argmax(logits, dim=1)
             correct += torch.sum(preds == labels[idx]).float()
             total += len(idx)
-    return (correct*100)/total
-    
+    return (correct * 100) / total
+
+
 def train(model, mlp, PCs, labels):
     print(args)
-    opt = torch.optim.AdamW(list(model.parameters())+list(mlp.parameters()), lr = args.lr, weight_decay = args.wd)
+    opt = torch.optim.AdamW(
+        list(model.parameters()) + list(mlp.parameters()),
+        lr=args.lr,
+        weight_decay=args.wd,
+    )
     train_idx, test_idx = train_test_split(np.arange(len(labels)), test_size=0.2)
     train_idx = torch.LongTensor(train_idx).to(args.device)
     test_idx = torch.LongTensor(test_idx).to(args.device)
@@ -75,33 +86,44 @@ def train(model, mlp, PCs, labels):
             t_loss = 0
             model.train()
             mlp.train()
-            for idx in (train_loader):
+            for idx in train_loader:
                 opt.zero_grad()
-                
+
                 X = model([PCs[i].to(args.device) for i in idx], args.sigma)
                 logits = mlp(X)
                 preds = torch.argmax(logits, dim=1)
-                correct_train += torch.sum(preds == labels[idx]).float() 
-                loss = loss_fn(logits, labels[idx])*100# + 0.1*(model.layer.alphas@model.layer.alphas.T - torch.eye(args.num_weights).to(args.device)).square().mean()
+                correct_train += torch.sum(preds == labels[idx]).float()
+                loss = (
+                    loss_fn(logits, labels[idx]) * 100
+                )  # + 0.1*(model.layer.alphas@model.layer.alphas.T - torch.eye(args.num_weights).to(args.device)).square().mean()
                 loss.backward()
                 for name, param in model.named_parameters():
                     if param.grad is not None:
-                        wandb.log({f"{name}.grad": param.grad.norm()}, step=epoch+1)
+                        wandb.log({f"{name}.grad": param.grad.norm()}, step=epoch + 1)
                 opt.step()
                 t_loss += loss.item()
-                del(X, logits, loss, preds)
+                del (X, logits, loss, preds)
                 torch.cuda.empty_cache()
                 gc.collect()
-                    
-            train_acc = correct_train*100/len(train_idx)
+
+            train_acc = correct_train * 100 / len(train_idx)
             test_acc = test(model, mlp, PCs, labels, test_loader)
-            wandb.log({'Loss':t_loss, 'Train acc':train_acc.item(), 'Test acc':test_acc.item()}, step=epoch+1)
+            wandb.log(
+                {
+                    "Loss": t_loss,
+                    "Train acc": train_acc.item(),
+                    "Test acc": test_acc.item(),
+                },
+                step=epoch + 1,
+            )
             # for k in range(len(model.layer.alphas)):
             #     for d in range(len(model.layer.alphas[k])):
             #         wandb.log({f'Alpha{k}_{d}':model.layer.alphas[k][d].item()}, step=epoch+1)
             if test_acc > best_acc:
                 best_acc = test_acc
-                model_path = args.raw_dir + f"/simplex_models/model_{args.num_weights}.pth"
+                model_path = (
+                    args.raw_dir + f"/simplex_models/model_{args.num_weights}.pth"
+                )
 
                 # torch.save({
                 #     'epoch': epoch,  # Save the current epoch number
@@ -111,13 +133,25 @@ def train(model, mlp, PCs, labels):
                 #     'best_acc': best_acc,
                 #     'args': args
                 # }, model_path)
-    
-            tq.set_description("Train Loss = %.4f, Train acc = %.4f, Test acc = %.4f, Best acc = %.4f" % (t_loss, train_acc.item(), test_acc.item(), best_acc))
+
+            tq.set_description(
+                "Train Loss = %.4f, Train acc = %.4f, Test acc = %.4f, Best acc = %.4f"
+                % (t_loss, train_acc.item(), test_acc.item(), best_acc)
+            )
     print(f"Best accuracy : {best_acc}")
-            
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
+    print(args)
+    import sys
+
+    sys.exit()
+    wandb.init(project="pointcloud-net-k-fold", config=vars(args))
+
     PCs, labels, num_labels = load_data(args.raw_dir, args.full)
-    model = HiPoNet(PCs[0].shape[1], args.num_weights, args.threshold, args.K, args.device)
+    model = HiPoNet(
+        PCs[0].shape[1], args.num_weights, args.threshold, args.K, args.device
+    )
     model = nn.DataParallel(model).to(args.device)
     with torch.no_grad():
         input_dim = model([PCs[0].to(args.device)], 1).shape[1]
@@ -130,5 +164,5 @@ if __name__ == '__main__':
     #     'best_acc': 0,
     #     'args': args
     # }, model_path)
-    
+
     train(model, mlp, PCs, labels)
