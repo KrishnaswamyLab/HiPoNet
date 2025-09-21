2fef414a7bf49dae479829306f730057d2246ca7
tlong <t.long@yale.edu>
2025-09-21 16:58:53 -0400

    use h200

--- classification_job.sh
+++ classification_job.sh
@@ -4,10 +4,10 @@
 #SBATCH --time=00:30:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=t.long@yale.edu
-#SBATCH --gpus=rtx_5000_ada:2
-#SBATCH --partition=gpu
+#SBATCH --h200:1
+#SBATCH --partition=gpu_h200
 
 source .venv/bin/activate
 uv run wandb login $WANDB_API_KEY
 echo "Starting classification job with arguments..." $@
-uv run main_classification.py --gpu=2 $@
+uv run main_classification.py --gpu=1 $@
